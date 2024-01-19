6db5eb8c660b0271712eea96557ff8d5a24761f1
Yuri Oliveira Gochi <yuri.gochi@ubc.org.br>
2024-01-19 14:56:10 -0300

    first commit

---
+++ Rabbit.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using Rabbit.Models.Entities;
+using System.Text.Json;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "admin",
+    Password = "123456"
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "rabbitMensagesQueue",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var json = Encoding.UTF8.GetString(body);
+
+        RabbitMessage mensagem = JsonSerializer.Deserialize<RabbitMessage>(json);
+
+        System.Threading.Thread.Sleep(1000);
+
+        Console.WriteLine($"Titulo: {mensagem.Titulo}; Texto={mensagem.Texto}; Id={mensagem.Id}");
+    };
+    channel.BasicConsume(queue: "rabbitMensagesQueue",
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
---
+++ Rabbit.Publisher.Api/Controllers/RabbitMessageControllers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rabbit.Models.Entities;
+using Rabbit.Services.Interfaces;
+
+namespace Rabbit.Publisher.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RabbitMessageControllers : ControllerBase
+    {
+        private readonly IRabbitMessageService _service;
+        public RabbitMessageControllers(IRabbitMessageService service) {
+            _service = service;
+        }
+
+        [HttpPost]
+        public void AddMensage(RabbitMessage message)
+        {
+            _service.SendMessage(message);
+        }
+
+    }
+}
